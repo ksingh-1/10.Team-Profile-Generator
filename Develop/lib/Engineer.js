0a0636d50ae1fb745fffab6a52121169cbe0dc87
SSVF2 <kulpreet.s18@gmail.com>
2020-08-20 15:07:50 -0500

    updating HW 10

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(name=null, id=null, email=null, gitHubUrl=null) {
+        super(name, id, email);
+        this.github=gitHubUrl;
+    }
+
+    getRole() {
+        return "Engineer";
+    }
+
+    getGithub(){
+        return this.github;
+    }
+}
+module.exports=Engineer;
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee=require("./Employee");
+
+class Intern extends Employee {
+    constructor(name=null, id=null, email=null, school=null) {
+        super(name, id, email);
+        this.school=school;
+    }
+
+    getRole(){
+        return this.school;
+    }
+
+    getSchool(){
+        return this.school;
+    }
+}
+module.exports=Intern;
